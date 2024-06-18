eab29191909bd4cf0df327f5a64eb2eed2a78425
mercier <chloe.mercier@imt-atlantique.fr>
2024-06-18 15:55:35 +0200

    modifié :         list_dir.py

--- list_dir.py
+++ list_dir.py
@@ -13,11 +13,11 @@ if __name__=="__main__":
 		sessions = os.listdir(subj_dir)
 		sessions.sort()
 		for session in sessions:
-            	dir_session = os.path.join(subj_dir, session)
-            	for sequence in sequences:
-            		print("--------------" + subject)
-            		dir_reconst = os.path.join(DB_path, "", subject, session, sequence)
-					stack = os.path.join(dir_reconst, subject+ "_"+ session + "_"+ "acq-"+ sequence+ "_"+ "run-1_desc-denoised_T2w.nii.gz")
-					print(stack)
+			dir_session = os.path.join(subj_dir, session)
+			for sequence in sequences:
+				print("--------------" + subject)
+				dir_reconst = os.path.join(DB_path, "", subject, session, sequence)
+				stack = os.path.join(dir_reconst, subject+ "_"+ session + "_"+ "acq-"+ sequence+ "_"+ "run-1_desc-denoised_T2w.nii.gz")
+				print(stack)
             			
                     
