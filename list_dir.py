bee7d92217c985d49c23fc3223c0676a3fb3376a
mercier <chloe.mercier@imt-atlantique.fr>
2024-06-19 11:37:17 +0200

    modifié :         ../list_dir.py

--- list_dir.py
+++ list_dir.py
@@ -6,6 +6,9 @@ if __name__=="__main__":
 	
 	subjects = os.listdir(DB_path)
 	subjects.sort()
+
+	list_stacks = []
+	list_masks = []
 	
 	for subject in subjects:
 		subj_dir = os.path.join(DB_path, subject)
@@ -18,8 +21,14 @@ if __name__=="__main__":
 			list_files = os.listdir(dir_reconst)
 			for file in list_files:
 				#print(file)
-				if file.endswith("_desc-denoised_T2w.nii.gz"):
+				if file.endswith("_desc-denoised_T2w.nii.gz") and 'haste' in file:
 					#stack = os.path.join(dir_reconst, subject+ "_"+ session + "_"+ "acq-"+ sequence+ "_"+ "run" + "-" + serie + "_desc-denoised_T2w.nii.gz")
-					print(file)
+					list_stacks.append(file)
+				elif file.endswith("_desc-brainmask_T2w.nii.gz"):
+					list_masks.append(file)
+		print('---stacks----')
+		print(list_stacks)
+		print('---masks----')
+		print(list_masks)
             			
                     
