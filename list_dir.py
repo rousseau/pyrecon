04eecc5d37e41b7b54d86d9ca2c258f537819965
mercier <chloe.mercier@imt-atlantique.fr>
2024-06-19 11:02:53 +0200

    modifié :         ../list_dir.py

--- list_dir.py
+++ list_dir.py
@@ -3,8 +3,7 @@ import os
 if __name__=="__main__":
 	
 	DB_path = "/envau/work/meca/data/Fetus/datasets/MarsFet/derivatives/preprocessing"
-	sequences = ["haste", "tru"]
-
+	
 	subjects = os.listdir(DB_path)
 	subjects.sort()
 	
@@ -14,14 +13,13 @@ if __name__=="__main__":
 		sessions.sort()
 		for session in sessions:
 			dir_session = os.path.join(subj_dir, session)
-			for sequence in sequences:
-				print("--------------" + subject)
-				dir_reconst = os.path.join(DB_path, "", subject, session, sequence)
-				list_files = os.listdir(dir_reconst)
-				for file in list_files:
+			print("--------------" + subject)
+			dir_reconst = os.path.join(DB_path, "", subject, session)
+			list_files = os.listdir(dir_reconst)
+			for file in list_files:
+				print(file)
+				if file.endswith("_desc-denoised_T2w.nii.gz"):
+					#stack = os.path.join(dir_reconst, subject+ "_"+ session + "_"+ "acq-"+ sequence+ "_"+ "run" + "-" + serie + "_desc-denoised_T2w.nii.gz")
 					print(file)
-					if file.endswith("_desc-denoised_T2w.nii.gz"):
-						#stack = os.path.join(dir_reconst, subject+ "_"+ session + "_"+ "acq-"+ sequence+ "_"+ "run" + "-" + serie + "_desc-denoised_T2w.nii.gz")
-						print(file)
             			
                     
