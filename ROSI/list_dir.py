a4b9bd2a2c34c6e8735e439254ad07dbd2673311
mercier <chloe.mercier@imt-atlantique.fr>
2024-06-20 16:14:23 +0200

    modifié :         ROSI/list_dir.py

--- ROSI/list_dir.py
+++ ROSI/list_dir.py
@@ -29,7 +29,8 @@ if __name__=="__main__":
 		#print(subject)
 		for session in sessions:
 				print(subject,session)
-				if ((subject,session)) in list_data:
+				if (subject,session) in list_data:
+					print('ici')
 					list_stacks = []
 					list_masks = []
 					dir_session = os.path.join(subj_dir, session)
