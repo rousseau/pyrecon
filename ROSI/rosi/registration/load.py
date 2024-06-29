b5911b3ab6508f6decf97b7089da9fea609a90b0
mercier <chloe.mercier@imt-atlantique.fr>
2024-06-29 19:38:42 +0200

    modifié :         ROSI/rosi/registration/load.py

--- ROSI/rosi/registration/load.py
+++ ROSI/rosi/registration/load.py
@@ -91,9 +91,9 @@ def loadStack(fileImage : str,
           print(stmask[np.where(stmask>0)])
           data = stmask.reshape(-1)
           stmask = Nifti1Image(stmask,stack.affine,dtype=np.int64)
-          data = stmask.get_fdata()
-          print(data[np.where(data>0)])
-          #print(data[data<1])
+          #data = stmask.get_fdata()
+          #print(data[np.where(data>0)])
+          print(data[data<1])
           #)
 
           if not (np.all((data==0)|(data==1))):
