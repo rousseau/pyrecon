6269a2d4c8a1907b9ea3d6dbdac9a9c63a57651e
mercier <chloe.mercier@imt-atlantique.fr>
2024-07-05 16:25:24 +0200

    ROSI/rosi/run_registration_with_svort.py 	ROSI/run_registration_svort.py

--- ROSI/rosi/registration/load.py
+++ ROSI/rosi/registration/load.py
@@ -8,9 +8,11 @@ Created on Tue Aug 23 09:20:15 2022
 
 from nibabel import Nifti1Image,load
 import numpy as np
+from rosi.reconstruction.rec_ebner import sorted_alphanumeric
 from .sliceObject import SliceObject
 from .tools import distance_from_mask
 from .sliceObject import SliceObject
+import os
 
 def convert2Slices(stack : Nifti1Image,
               mask : Nifti1Image,
@@ -99,3 +101,83 @@ def loadStack(fileImage : str,
           if not (np.all((data==0)|(data==1))):
                raise Exception('The mask is not a binary image')
     return stack,stmask
+
+
+def loadFromdir(dir_input):
+     
+     """
+     Create a listofSlice from the direcory containing all the slices
+     """
+
+     list_file = sorted_alphanumeric(os.listdir(dir_input))
+     OutputList = []
+     list_stack_ortho = []
+     list_stack_zi = []
+     for file in list_file:
+          if not "mask" in file:
+            slice_path = os.path.join(dir_input,slice_path)
+            nib_slice = load(slice_path)
+            mask_path=os.path.join(dir_input,'/mask_' + file)
+            nib_mask = load(mask_path)
+            
+            new_stack = False
+            for i in range(0,len(list_stack_ortho)):
+                vec_orthogonal = np.norm(nib_slice.affine[0:3,0] @ nib_slice.affine[0:3,1])
+                if np.abs(np.cross(vec_orthogonal,list_stack_ortho[i])) < 0.75 :
+                    index_stack = i
+                    break
+                else : 
+                    if i == len(list_stack_ortho):
+                        new_stack = True
+                        index_stack = i +1
+            if new_stack : 
+                list_stack_ortho.append(vec_orthogonal)
+                list_stack_zi.append(1)
+
+            zi = list_stack_zi[index_stack]+1
+            list_stack_zi[index_stack]=list_stack_zi[index_stack]+1
+            index_volume = index_stack
+            new_object = SliceObject(nib_slice,nib_mask.get_fdata(),index_stack,zi,index_volume)
+            OutputList.append(new_object)
+
+     return OutputList
+
+
+
+def convert2ListSlice(dir_nomvt,dir_slice,slice_thickness,set_of_affines):
+    #print(dir_nomvt)
+    ##this function take each slices, saved in a nifti, and convert it to sliceObject. It then create a listOfSliceObject, which are used to compute the tre.
+    list_file = sorted_alphanumeric(os.listdir(dir_slice))
+    #print(list_file)
+
+    listSlice=[];listnomvt=[]
+    index=np.zeros(3,np.int32)
+    for file in list_file:
+        #check that the file is a slice
+        
+        if not 'mask' in file and not 'image' in file and not 'volume' in file : ##for svrtk you would need to have "slice"
+            ##else do nothing
+            print('file_name :',file)
+            slice_data=nib.load(dir_slice + '/' + file)
+            mask_data=nib.load(dir_slice + '/mask_' + file)
+            slice_nomvt = nib.load(dir_nomvt + '/' + file)
+            mask_slice=nib.load(dir_slice + '/mask_' + file)
+            print("affine equal ? ",np.allclose(slice_data.affine,slice_nomvt.affine,1e-1))
+                #print(slice_data.affine - slice_nomvt.affine)
+            print(slice_data.affine)
+            print(slice_nomvt.affine)
+                
+            T0 = slice_data.affine
+
+
+            num_stack=which_stack(slice_nomvt.affine,slice_thickness) 
+                #print(slice_data.affine)
+            stack_affine=set_of_affines[num_stack]
+            
+            i_slice=where_in_the_stack(stack_affine,slice_nomvt.affine,num_stack)
+            slicei = SliceObject(slice_data,mask_data.get_fdata(),num_stack,i_slice, num_stack)
+            slicen = SliceObject(slice_nomvt,mask_slice.get_fdata(),num_stack,i_slice, num_stack)
+                
+            listSlice.append(slicei)
+            listnomvt.append(slicen)
+    return listnomvt,listSlice
