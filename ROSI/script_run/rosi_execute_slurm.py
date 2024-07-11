e75337f1d045b2da06162fb3e939f674de393494
mercier <chloe.mercier@imt-atlantique.fr>
2024-07-11 10:03:00 +0200

    modifié :         ROSI/script_run/rosi_execute_slurm.py 	modifié :         ROSI/utils/slurm/rosi.slurm

--- ROSI/script_run/rosi_execute_slurm.py
+++ ROSI/script_run/rosi_execute_slurm.py
@@ -4,46 +4,9 @@ import pathlib
 import subprocess
 import csv
 
-def get_identifier(filepath, pattern):
-    """Get project identifier(s)"""
-    filename = os.path.basename(filepath)
-    regex = re.compile(pattern)
-    match = regex.search(filename)
-    if match is not None:
-        identifier = match.group()
-    else:
-        identifier = None
-    return identifier
-
-
-def execute_rosi(input_slices,output_dir,slurm_config):
-    """
-    Execute ROSI, with svort initialisation
-    :filenames: dir input with individual slices, in an nifti file
-    :output: output directory
-    :return: None
-    """
-    dir_out = pathlib.Path(output_dir)
-    dir_out.mkdir(parents=True, exist_ok=True)
-
-    dir_in = pathlib.Path(input_slices)
-
-
-    if os.path.exists(dir_in):
-        cmd = [
-            "sbatch",
-            slurm_config,
-            dir_in,
-            dir_out,
-            ]
-        subprocess.run(cmd)
-    pass
 
 if __name__ == "__main__":
 
-    rosi_slurm = (
-        "/scratch/cmercier/code/pyrecon/ROSI/utils/slurm/rosi.slurm"
-    )
 
     MARSFET_DATAPATH = "/scratch/gauzias/data/datasets/MarsFet/derivatives/preprocessing"  
 
@@ -71,10 +34,19 @@ if __name__ == "__main__":
         dir_subject = os.path.join(stacks_path, subject)
         sessions = os.listdir(dir_subject)
         for session in sessions:
-            if subject in sub_list and session in ses_list :
+            if subject == "sub-0002" and session == "ses-0002":
+            #in sub_list and session in ses_list :
                 input_slices = os.path.join(input_data,subject, session, 'res')
                 dir_out = os.path.join(output_data, subject, session)
                 print('input_slices:',input_slices)
                 print('dir_output:',dir_out)
-                execute_rosi(input_slices,dir_out,rosi_slurm)
+                cmd = (
+                    "sbatch"
+                    + " "
+                    + "/scratch/cmercier/code/pyrecon/ROSI/utils/slurm/rosi.slurm"
+                    + " "
+                    + input_slices
+                    + " "
+                    + dir_out
+                    )
 
