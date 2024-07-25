c175fff7b1c882343d645d7faac0d6dab95a91f5
mercier <chloe.mercier@imt-atlantique.fr>
2024-07-25 10:41:46 +0200

    modifié :         ROSI/script_run/nesvor_execute_slurm.py

--- ROSI/script_run/nesvor_execute_slurm.py
+++ ROSI/script_run/nesvor_execute_slurm.py
@@ -68,7 +68,9 @@ if __name__ == "__main__":
                     cmd_os_2 += " --no-transformation-optimization "
                     cmd_os_2 += " --inference-batch-size 256 "
                     cmd_os_2 += " --n-inference-samples 128 "
-                    cmd_os_2 += " --single-precision "
+                    cmd_os_2 += "  --output-resolution 0.5 "
+                    #cmd_os_2 += " --single-precision "
+                    
 
                     cmd = (
                         "sbatch"
