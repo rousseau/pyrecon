dd21985068b0765f65231f10dc44953ae7dcd915
mercier <chloe.mercier@imt-atlantique.fr>
2024-09-10 09:33:44 +0200

    modifié :         ROSI/script_run/nesvor_execute_slurm.py

--- ROSI/script_run/nesvor_execute_slurm.py
+++ ROSI/script_run/nesvor_execute_slurm.py
@@ -53,9 +53,9 @@ if __name__ == "__main__":
                 #print("input_stacks",input_stacks)
             
                 
-                output_svort = os.path.join(output,'nesvor', 'rosi_outliers', subject, session)
-                output_svort_mask = os.path.join(output,'nesvor', 'rosi_outliers_mask', subject, session)
-                output_svort_similarity = os.path.join('/data','nesvor', 'rosi_outliers', subject, session)
+                output_svort = os.path.join(output,'nesvor', 'rosi_all_slices', subject, session)
+                output_svort_mask = os.path.join(output,'nesvor', 'rosi_all_slices_mask', subject, session)
+                output_svort_similarity = os.path.join('/data','nesvor', 'rosi_all_slices', subject, session)
                 output_nesvor = os.path.join('/data','nesvor',subject,session,"volume_rosi_outliers.nii")
                 path_to_mask = os.path.join(output,'nesvor',subject,session,"volume_mask.nii")
                 path_to_volume = os.path.join(output,'nesvor',subject,session,"volume_rosi_outliers.nii")
