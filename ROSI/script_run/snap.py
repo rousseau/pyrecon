d4d6f2168adf8b16cdaaaed0b42a75ca4a2883a7
mercier <chloe.mercier@imt-atlantique.fr>
2024-09-12 16:09:33 +0200

    modifié :         ROSI/script_run/snap.py

--- ROSI/script_run/snap.py
+++ ROSI/script_run/snap.py
@@ -48,13 +48,13 @@ if __name__ == "__main__":
             #== "sub-0002" and session == "ses-0002":
 
                 path_to_mask = os.path.join(output,'nesvor',subject,session,"volume_mask.nii")
-                path_to_volume = os.path.join(output,'nesvor',subject,session,"volume_rosi_outliers.nii")
+                path_to_volume = os.path.join(output,'nesvor',subject,session,"volume_rosi_outliers_08.nii")
                
                 if  os.path.exists(path_to_volume):
 
                     #save in nisnap simple visualisation
                     #code issue de MarsFet/fet-processing
-                    prefix_output = os.path.join(output,'snap2','outliers',subject,session)
+                    prefix_output = os.path.join(output,'snap2','outliers_08',subject,session)
                     figsize = {'x': (18, 4), 'y': (18, 4), 'z': (18, 5)}
                     if not os.path.exists(prefix_output):
                         os.makedirs(prefix_output)
