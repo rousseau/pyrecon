693b5027aed220b85d85171a0d68a9bf94a82940
mercier <chloe.mercier@imt-atlantique.fr>
2024-09-12 09:48:35 +0200

    modifié :         ROSI/convert_to_svort.py 	modifié :         ROSI/script_run/nesvor_execute_slurm_alone.py

--- ROSI/script_run/nesvor_execute_slurm_alone.py
+++ ROSI/script_run/nesvor_execute_slurm_alone.py
@@ -85,12 +85,12 @@ if __name__ == "__main__":
                 #print("input_stacks",input_stacks)
             
                 
-                output_svort = os.path.join(output,'nesvor', 'rosi_all_slices', subject, session)
-                output_svort_mask = os.path.join(output,'nesvor', 'rosi_all_slices_mask', subject, session)
+                output_svort = os.path.join(output,'nesvor', 'rosi_outliers', subject, session)
+                output_svort_mask = os.path.join(output,'nesvor', 'rosi_outliers_mask', subject, session)
                 output_svort_similarity = os.path.join('/data','nesvor', 'rosi_outliers', subject, session)
-                output_nesvor = os.path.join('/data','nesvor',subject,session,"volume_rosi_all_slices.nii")
+                output_nesvor = os.path.join('/data','nesvor',subject,session,"volume_rosi_outliers.nii")
                 path_to_mask = os.path.join(output,'nesvor',subject,session,"volume_mask.nii")
-                path_to_volume = os.path.join(output,'nesvor',subject,session,"volume_rosi_all_slices.nii")
+                path_to_volume = os.path.join(output,'nesvor',subject,session,"volume_rosi_outliers.nii")
                
                 if True : 
                 #os.path.exists(joblib_path) and not os.path.exists(path_to_volume):
