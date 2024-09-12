b97ac507e8419b00ad0d15678ef216c114c57424
mercier <chloe.mercier@imt-atlantique.fr>
2024-09-12 10:52:48 +0200

    modifié :         ROSI/script_run/nesvor_execute_slurm_alone.py

--- ROSI/script_run/nesvor_execute_slurm_alone.py
+++ ROSI/script_run/nesvor_execute_slurm_alone.py
@@ -41,8 +41,9 @@ if __name__ == "__main__":
         sessions = os.listdir(dir_subject)
         for session in sessions:
             #if  :
-            if subject == "sub-0102" and session == "ses-0118":
-            #subject in sub_list and session in ses_list : 
+            if subject in sub_list and session in ses_list : 
+            #subject == "sub-0102" and session == "ses-0118":
+            #
             #
 
                 input_stacks = os.listdir(os.path.join(stacks_path,subject, session))
