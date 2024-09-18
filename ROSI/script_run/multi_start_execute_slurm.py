4edbf0ee777f8b91ab88b4b360eaf6d8e1c98078
mercier <chloe.mercier@imt-atlantique.fr>
2024-09-18 15:44:22 +0200

    modifié :         ROSI/script_run/multi_start_execute_slurm.py

--- ROSI/script_run/multi_start_execute_slurm.py
+++ ROSI/script_run/multi_start_execute_slurm.py
@@ -39,7 +39,7 @@ if __name__ == "__main__":
             #
             #subject== "sub-0001" and session == "ses-0001":
             #if subject in sub_list and session in ses_list :
-            subject=="sub-0263" and session == "ses-0312":
+            if subject=="sub-0263" and session == "ses-0312":
             #subject in sub_list and session in ses_list :
             #subject== "sub-0047" and session == "ses-0057":
                 input_stacks = os.listdir(os.path.join(stacks_path,subject, session))
