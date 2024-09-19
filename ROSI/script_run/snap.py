04d99722b9fe9beaec9663e1819233ead80295b2
mercier <chloe.mercier@imt-atlantique.fr>
2024-09-19 11:06:49 +0200

    modifié :         ROSI/script_run/snap.py

--- ROSI/script_run/snap.py
+++ ROSI/script_run/snap.py
@@ -41,7 +41,7 @@ if __name__ == "__main__":
         dir_subject = os.path.join(stacks_path, subject)
         sessions = os.listdir(dir_subject)
         for session in sessions:
-            if subject == "sub-0009" and session == "ses-0012":
+            if subject == "sub-0002" and session == "ses-0002":
             #in sub_list and session in ses_list :
             #
             #
