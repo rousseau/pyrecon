3c43c1bd12180e90103dee84157174f856561a1a
mercier <chloe.mercier@imt-atlantique.fr>
2024-09-19 17:01:35 +0200

    modifié :         ROSI/script_run/svort_execute_slurm.py

--- ROSI/script_run/svort_execute_slurm.py
+++ ROSI/script_run/svort_execute_slurm.py
@@ -33,8 +33,9 @@ if __name__ == "__main__":
         dir_subject = os.path.join(stacks_path, subject)
         sessions = os.listdir(dir_subject)
         for session in sessions:
-            if subject== "sub-0002" and session == "ses-0002":
-            #subject in sub_list and session in ses_list : 
+            if subject in sub_list and session in ses_list :
+            #subject== "sub-0002" and session == "ses-0002":
+            # 
             #subject== "sub-0117" and session == "ses-0137":
             #
             #subject in sub_list and session in ses_list :
@@ -43,7 +44,7 @@ if __name__ == "__main__":
                 list_stacks=[]
                 list_masks=[]
                 for file in input_stacks:
-                    print(file)
+                    #print(file)
                     if file.endswith("denoised_T2w.nii.gz") and 'tru' in file:
 						#stack = os.path.join(dir_reconst, subject+ "_"+ session + "_"+ "acq-"+ sequence+ "_"+ "run" + "-" + serie + "_desc-denoised_T2w.nii.gz")
                         path_to_file = os.path.join('/data',subject,session,file)
