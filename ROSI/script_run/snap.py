1e4883701213e2b2dbe16b1b94371cf320b767aa
mercier <chloe.mercier@imt-atlantique.fr>
2024-09-24 10:07:10 +0200

    modifié :         ROSI/script_run/snap.py

--- ROSI/script_run/snap.py
+++ ROSI/script_run/snap.py
@@ -46,9 +46,9 @@ if __name__ == "__main__":
             #== "sub-0002" and session == "ses-0002":
 
                 path_to_mask = os.path.join(output,'nesvor',subject,session,"volume_mask.nii")
-                #path_to_reconstruction = "/scratch/gauzias/data/datasets/MarsFet/derivatives/srr_reconstruction/latest_nesvor/"
-                path_to_volume = os.path.join(output,'nesvor',subject,session,"volume_rosi_tru.nii")
-                #path_to_volume = os.path.join(path_to_reconstruction,subject,session,"haste/default_reconst/","%s_%s_acq-haste_rec-nesvor_T2w.nii.gz" %(subject,session))
+                path_to_reconstruction = "/scratch/gauzias/data/datasets/MarsFet/derivatives/srr_reconstruction/latest_nesvor/"
+                #path_to_volume = os.path.join(output,'nesvor',subject,session,"volume_rosi_tru.nii")
+                path_to_volume = os.path.join(path_to_reconstruction,subject,session,"tru/default_reconst/","%s_%s_acq-haste_rec-nesvor_T2w.nii.gz" %(subject,session))
                 #path_to_mask = os.path.join(path_to_reconstruction,subject,session,"haste/default_reconst/", "%s_%s_acq-haste_rec-nesvor_desc-brainmask_T2w.nii.gz" %(subject,session))
 
                 if  os.path.exists(path_to_volume):
@@ -56,7 +56,7 @@ if __name__ == "__main__":
                     print(path_to_volume)
                     #save in nisnap simple visualisation
                     #code issue de MarsFet/fet-processing
-                    prefix_output = os.path.join(output,'snap2','truf',subject,session)
+                    prefix_output = os.path.join(output,'snap2','truf-nesvor',subject,session)
                     figsize = {'x': (18, 4), 'y': (18, 4), 'z': (18, 5)}
                     if not os.path.exists(prefix_output):
                         os.makedirs(prefix_output)
