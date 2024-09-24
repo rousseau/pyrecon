c51d9f6a3c1ddfee6812802a88fef52e29e013e1
mercier <chloe.mercier@imt-atlantique.fr>
2024-09-24 14:04:35 +0200

    modifié :         ROSI/script_run/rosi_execute_slurm.py

--- ROSI/script_run/rosi_execute_slurm.py
+++ ROSI/script_run/rosi_execute_slurm.py
@@ -35,7 +35,7 @@ if __name__ == "__main__":
         sessions = os.listdir(dir_subject)
         for session in sessions:
             #if subject in sub_list and session in ses_list :
-            if subject == "sub-0567" and session == "ses-0681":
+            if subject == "sub-0662" and session == "ses-0788":
             #
             #
             #
