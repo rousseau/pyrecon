bf1b541c3644b052a75cc644f2276b9945ad4807
mercier <chloe.mercier@imt-atlantique.fr>
2024-09-24 11:37:06 +0200

    modifié :         ROSI/script_run/nesvor_execute_slurm.py

--- ROSI/script_run/nesvor_execute_slurm.py
+++ ROSI/script_run/nesvor_execute_slurm.py
@@ -41,8 +41,8 @@ if __name__ == "__main__":
         sessions = os.listdir(dir_subject)
         for session in sessions:
             #if  :
-            #if  subject == "sub-0703" and session == "ses-0829":
-            if subject in sub_list and session in ses_list :
+            if  subject == "sub-0567" and session == "ses-0681":
+            #if subject in sub_list and session in ses_list :
             #
             #subject in sub_list and session in ses_list :
             #
