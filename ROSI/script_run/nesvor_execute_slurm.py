1155c436b7e2080ac0cc204f5a86baf50a4dad76
mercier <chloe.mercier@imt-atlantique.fr>
2024-09-25 15:39:19 +0200

    modifié :         ROSI/script_run/nesvor_execute_slurm.py

--- ROSI/script_run/nesvor_execute_slurm.py
+++ ROSI/script_run/nesvor_execute_slurm.py
@@ -53,7 +53,7 @@ if __name__ == "__main__":
             #
             #== "sub-0009" and session == "ses-0012":
             #== "sub-0002" and session == "ses-0002":
-                joblib_path = os.path.join(job_res,subject, session, 'res','res.joblib.gz')
+                joblib_path = os.path.join(job_res,subject, session,'res.joblib.gz')
                 print(joblib_path)
                 input_slices = os.path.join(slices_path,subject,session,'res')
                 #print(input_sl)
