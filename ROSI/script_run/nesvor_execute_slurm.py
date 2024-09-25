178a80bc17a4969504948f3f0c8001c0d93f464e
mercier <chloe.mercier@imt-atlantique.fr>
2024-09-25 11:18:24 +0200

    modifié :         ROSI/script_run/nesvor_execute_slurm.py

--- ROSI/script_run/nesvor_execute_slurm.py
+++ ROSI/script_run/nesvor_execute_slurm.py
@@ -67,7 +67,7 @@ if __name__ == "__main__":
                 path_to_mask = os.path.join(output,'nesvor',subject,session,"volume_mask.nii")
                 path_to_volume = os.path.join(output,'nesvor',subject,session,"volume_rosi_clinique.nii")
                
-                if cliniquee : 
+                if True : 
                 #if os.path.exists(joblib_path) and not os.path.exists(path_to_volume):
                     cmd_os_1 = " --input_slices " + input_slices
                     cmd_os_1 += " --output " + output_svort
