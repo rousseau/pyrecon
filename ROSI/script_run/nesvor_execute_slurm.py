6d0adb791e9cb0687de0c8b1857475f9bd728cc4
mercier <chloe.mercier@imt-atlantique.fr>
2024-09-26 11:51:02 +0200

    modifié :         ROSI/script_run/nesvor_execute_slurm.py

--- ROSI/script_run/nesvor_execute_slurm.py
+++ ROSI/script_run/nesvor_execute_slurm.py
@@ -53,19 +53,19 @@ if __name__ == "__main__":
             #
             #== "sub-0009" and session == "ses-0012":
             #== "sub-0002" and session == "ses-0002":
-                joblib_path = os.path.join(job_res,subject, session,'res.joblib.gz')
+                joblib_path = os.path.join(job_res,subject, session,'res_alone','res.joblib.gz')
                 print(joblib_path)
                 input_slices = os.path.join(slices_path,subject,session,'res')
                 #print(input_sl)
                 #print("input_stacks",input_stacks)
             
                 
-                output_svort = os.path.join(output,'nesvor', 'rosi_all_slices', subject, session)
-                output_svort_mask = os.path.join(output,'nesvor', 'rosi_all_slices_mask', subject, session)
-                output_svort_similarity = os.path.join('/data','nesvor', 'rosi_all_slices', subject, session)
-                output_nesvor = os.path.join('/data','nesvor',subject,session,"volume_rosi_all_slices.nii")
+                output_svort = os.path.join(output,'nesvor', 'rosi_outliers', subject, session)
+                output_svort_mask = os.path.join(output,'nesvor', 'rosi_outliers_mask', subject, session)
+                output_svort_similarity = os.path.join('/data','nesvor', 'rosi_outliers', subject, session)
+                output_nesvor = os.path.join('/data','nesvor',subject,session,"volume_rosi_outliers.nii")
                 path_to_mask = os.path.join(output,'nesvor',subject,session,"volume_mask.nii")
-                path_to_volume = os.path.join(output,'nesvor',subject,session,"volume_rosi_all_slices.nii")
+                path_to_volume = os.path.join(output,'nesvor',subject,session,"volume_rosi_outliers.nii")
                
                 if True : 
                 #if os.path.exists(joblib_path) and not os.path.exists(path_to_volume):
