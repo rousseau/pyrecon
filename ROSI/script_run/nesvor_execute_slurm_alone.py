131edb46d16c1f0003601b8c77da3f41686b0c9a
mercier <chloe.mercier@imt-atlantique.fr>
2024-09-30 10:11:29 +0200

    modifié :         ROSI/script_run/nesvor_execute_slurm_alone.py

--- ROSI/script_run/nesvor_execute_slurm_alone.py
+++ ROSI/script_run/nesvor_execute_slurm_alone.py
@@ -114,7 +114,7 @@ if __name__ == "__main__":
                     cmd_os_2 += " --output-resolution 0.5"
                     cmd_os_2 += " --inference-batch-size 255 "
                     cmd_os_2 += " --n-inference-samples 128 "
-                    cmd_os_2 += " --single-precision "
+                    #cmd_os_2 += " --single-precision "
                     
 
                     cmd = (
