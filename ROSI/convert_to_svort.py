259884ce106fa7b873340b288f7337a3d221217f
mercier <chloe.mercier@imt-atlantique.fr>
2024-10-01 09:32:24 +0200

    modifié :         ROSI/convert_to_svort.py

--- ROSI/convert_to_svort.py
+++ ROSI/convert_to_svort.py
@@ -296,7 +296,7 @@ if __name__ == '__main__':
         affine = islice.get_estimatedTransfo()
         sliceoriginal = sliceor.get_slice().get_fdata() * mask
         dataslice = sliceoriginal
-        dataslice = sliceoriginal / np.quantile(sliceoriginal,0.99)
+        #dataslice = sliceoriginal / np.quantile(sliceoriginal,0.99)
         nibslice = nib.Nifti1Image((dataslice),affine)
         nibslice.header.set_data_dtype(np.float32)
         nibmask = nib.Nifti1Image(mask,affine)
