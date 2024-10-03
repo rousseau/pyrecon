83b59af6c88c464c67b5cc4488b2ba1b0ee8a176
mercier <chloe.mercier@imt-atlantique.fr>
2024-10-03 15:29:58 +0200

    modifié :         ROSI/rosi/registration/outliers_detection/feature.py 	modifié :         ROSI/script_run/rosi_execute_slurm.py

--- ROSI/rosi/registration/outliers_detection/feature.py
+++ ROSI/rosi/registration/outliers_detection/feature.py
@@ -118,6 +118,7 @@ def update_features(listOfSlice : 'list[SliceObject]',
     """
     
     stacks,masks=separate_slices_in_stacks(listOfSlice)
+    print(len(stacks))
     variance = [compute_noise_variance(img) for img in stacks]
     index = [len(masks[n]) for n in range(0,len(masks))]
     pmasktot=[max(sum(masks[n][0:index[n]],axis=(1,2))) for n in range(0,len(masks))]
