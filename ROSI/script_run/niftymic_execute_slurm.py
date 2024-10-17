fce2126e5c09fe580102bfddd02c9f6e660681cb
mercier <chloe.mercier@imt-atlantique.fr>
2024-10-17 10:44:27 +0200

    modifié :         ROSI/script_run/niftymic_execute_slurm.py

--- ROSI/script_run/niftymic_execute_slurm.py
+++ ROSI/script_run/niftymic_execute_slurm.py
@@ -49,7 +49,7 @@ if __name__ == "__main__":
                 for file in input_stacks:
                     if file.endswith("denoised_T2w.nii.gz") and 'tru' in file:
 						#stack = os.path.join(dir_reconst, subject+ "_"+ session + "_"+ "acq-"+ sequence+ "_"+ "run" + "-" + serie + "_desc-denoised_T2w.nii.gz")
-                        path_to_file = os.path.join(stacks_path,subject,session,file)
+                        path_to_file = os.path.join('/data',subject,session,file)
                         list_stacks.append(path_to_file)
                         run = file.find("run") #find the number of the run to make sure the stack is associated with its corresponding mask
                         desc = file.find("_desc")
@@ -58,7 +58,7 @@ if __name__ == "__main__":
                         num = "run-%s_" %(file[num_index_min:num_index_max])
                         for file in input_stacks:
                             if file.endswith("brainmask_T2w.nii.gz") and 'tru' in file and num in file:
-                                path_to_file = os.path.join(stacks_path,subject,session,file)
+                                path_to_file = os.path.join('/data',subject,session,file)
                                 list_masks.append(path_to_file)
                                 break
                 print(list_stacks)
