4e844978314f23c662f27963a1055149b097c5e3
mercier <chloe.mercier@imt-atlantique.fr>
2024-11-07 10:48:37 +0100

    modifié :         script_run/nesvor_execute_slurm.py

--- ROSI/script_run/nesvor_execute_slurm.py
+++ ROSI/script_run/nesvor_execute_slurm.py
@@ -70,7 +70,8 @@ if __name__ == "__main__":
                 path_to_mask = os.path.join(output,'nesvor',subject,session,"volume_mask.nii")
                 path_to_volume = os.path.join(output,'nesvor',subject,session,"volume_rosi_tru.nii")
                
-                #if True : 
+                #if True :
+                print(os.path.exists(joblib_path) and not os.path.exists(path_to_volume)) 
                 if os.path.exists(joblib_path) and not os.path.exists(path_to_volume):
                     cmd_os_1 = " --input_slices " + input_slices
                     cmd_os_1 += " --output " + output_tru
