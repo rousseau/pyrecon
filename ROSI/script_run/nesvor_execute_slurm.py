1e71dc74afa19749485a0d40ecea32dfb1140419
mercier <chloe.mercier@imt-atlantique.fr>
2024-12-16 13:52:53 +0100

    modifié :         ROSI/script_run/nesvor_execute_slurm.py

--- ROSI/script_run/nesvor_execute_slurm.py
+++ ROSI/script_run/nesvor_execute_slurm.py
@@ -43,8 +43,8 @@ if __name__ == "__main__":
             #if  :
             #if  subject == "sub-0148" and session == "ses-0174":
             #    print("this subject exist hehehe")
-            #if (subject == "sub-0102" and session == "ses-0118") or (subject == "sub-0567" and session == "ses-0681") or (subject == "sub-0327" and session == "ses-0396") or (subject == "sub-0649" and session == "ses-0775") or (subject == "sub-0278" and session == "ses-1098") :
-            if subject in sub_list and session in ses_list :
+            if subject == "sub-0379"  or subject == "sub-0567" or subject == "sub-0703" :
+            #if subject in sub_list and session in ses_list :
             #
             #
             #
@@ -73,7 +73,8 @@ if __name__ == "__main__":
                 #if True :
                 print("sub :",subject," ses :",session)
                 print(os.path.exists(joblib_path),not os.path.exists(path_to_volume)) 
-                if os.path.exists(joblib_path) and not os.path.exists(path_to_volume):
+                if True:
+                #os.path.exists(joblib_path) and not os.path.exists(path_to_volume):
                     cmd_os_1 = " --input_slices " + input_slices
                     cmd_os_1 += " --output " + output_tru
                     cmd_os_1 += " --output_mask " + output_tru_mask
